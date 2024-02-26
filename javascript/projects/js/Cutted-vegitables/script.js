1b5cac026d0a2205486d66b473043de2b7e9b3ea
KVNarayana9k <157136802+KVNarayana9k@users.noreply.github.com>
2024-02-26 14:23:43 +0530

    vegitables

---
+++ javascript/projects/js/Cutted-vegitables/script.js
@@ -0,0 +1,262 @@
+const data = [
+  {
+    Title: "Carrot",
+    Prise: "$35",
+    Weight: "1kg",
+    Poster:
+      "https://th.bing.com/th/id/OIP.YvKrlhj6fz3u8b174n4BoAHaE8?w=280&h=187&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    Images: [
+      "https://th.bing.com/th/id/OIP.QJmrJpYOSC6GOzxdVxLvfQHaFj?w=234&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+      "https://th.bing.com/th/id/OIP.kPmIxYjsNDK7NDe2QPIQ0QHaE8?w=274&h=191&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    ],
+  },
+  {
+    Title: "Watermelon",
+    Prise: "$25.37",
+    Weight: "1kg",
+    Poster:
+      "https://th.bing.com/th/id/OIP.sizN1YWC_3Ht-KKTzuSeRAHaFT?w=257&h=184&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    Images: [
+      "https://th.bing.com/th/id/OIP.sizN1YWC_3Ht-KKTzuSeRAHaFT?w=257&h=184&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    ],
+  },
+  {
+    Title: "Pineapple",
+    Prise: "$40.05",
+    Weight: "1kg",
+    Poster:
+      "https://th.bing.com/th/id/OIP.tGp3RFwUtWIXGiLYxyj3ygHaD9?w=306&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    Images: [
+      "https://th.bing.com/th/id/OIP.BedL_NaKQziwxzvFOC0k7QHaJ4?w=130&h=190&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+      "https://th.bing.com/th/id/OIP.FdZqlSNSCnv0zHdkeHLa4gHaE8?w=293&h=195&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    ],
+  },
+
+  {
+    Title: "Apple",
+    Prise: "$150.79",
+    Weight: "1kg",
+    Poster:
+      "https://th.bing.com/th/id/OIP.U7bJ4NS3dGjtWSZBNVWcDgHaE6?w=277&h=184&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    Images: [
+      "https://th.bing.com/th/id/OIP.oYXcGTMFI0_J3vPbrxKH9gHaE7?w=277&h=184&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    ],
+  },
+
+  {
+    Title: "Sapota",
+    Prise: "$40.00",
+    Weight: "1kg",
+    Poster:
+      "https://th.bing.com/th/id/OIP.3aQZ8mCCdzbZPjSsyXpHSgHaFj?w=227&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    Images: [
+      "https://th.bing.com/th/id/OIP.vdFKllYBNJXLMKfglEp1IQHaEJ?w=334&h=187&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    ],
+  },
+
+//   {
+//     Title: "Mango",
+//     Prise: "$137.46",
+//     Weight: "1.1/2kg",
+//     Poster:
+//       "https://i.pinimg.com/originals/e7/c2/3b/e7c23b7b29f41aa1b49ab568e53f4372.jpg",
+//     Images: [
+//       "https://www.bing.com/th/id/OIP.d3qS0ubtspbsN-yNJdLPGgAAAA?w=214&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+//     ],
+//   },
+   {
+    Title: "Stawberry",
+    Prise: "$137.46",
+    Weight: "1/2kg",
+    Poster:
+      "https://th.bing.com/th/id/OIP.gE5L45DgTb0VzPxVZx8GrQAAAA?w=334&h=271&rs=1&pid=ImgDetMain",
+    Images: [
+      "https://th.bing.com/th/id/OIP.GG0rVheC6NUHUc3CBQrivAHaHa?pid=ImgDet&w=167&h=167&c=7&dpr=1.3",
+    ],
+  },
+  {
+    Title: "Banana",
+    Prise: "$65.30",
+    Weight: "12pices",
+    Poster:
+      "https://th.bing.com/th/id/OIP.KImACA3UWbgvDW_1gPAZrwHaEK?w=254&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    Images: [
+      "",
+    ],
+  },
+
+//   {
+//     Title: "Baby Kiwi",
+//     Prise: "$100.46",
+//     Weight: "4pices",
+//     Poster:
+//       "https://freepngimg.com/thumb/kiwi/24-green-cutted-kiwi-png-image.png",
+//     Images: [
+//       "https://freepngimg.com/thumb/kiwi/24-green-cutted-kiwi-png-image.png",
+//     ],
+//   },
+
+  {
+    Title: "Tomato",
+    Prise: "$50.35",
+    Weight: "1kg",
+    Poster:
+      "https://th.bing.com/th/id/OIP.6T88CvgN3q_E5kIzqViACwHaHf?w=198&h=200&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    Images: [
+      "https://th.bing.com/th/id/OIP.6T88CvgN3q_E5kIzqViACwHaHf?w=198&h=200&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    ],
+  },
+
+  {
+    Title: "Capsicum",
+    Prise: "$58.69",
+    Weight: "1kg",
+    Poster:
+      "https://th.bing.com/th/id/OIP.nv-Bnb6wdD73xFc88zlAZwHaGc?w=228&h=199&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    Images: [
+      "",
+    ],
+  },
+
+  {
+    Title: "Lemon",
+    Prise: "$60.00",
+    Weight: "12pices",
+    Poster:
+      "https://th.bing.com/th/id/OIP.lWSZmomxjYYCSdprfuMZXQHaEn?w=276&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    Images: [
+      "https://th.bing.com/th/id/OIP.i1HRcWqdC5iHv0nRQsNqQgHaGT?w=202&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    ],
+  },
+  {
+    Title: "Papaya",
+    Prise: "$50.00",
+    Weight: "1kg",
+    Poster:
+      "https://th.bing.com/th/id/OIP.iV5F_58desK40qxwM8Wf3gHaFj?w=225&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    Images: [
+      "https://th.bing.com/th/id/OIP.iV5F_58desK40qxwM8Wf3gHaFj?w=225&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    ],
+  },
+  {
+    Title: "Guava",
+    Prise: "$60.00",
+    Weight: "1kg",
+    Poster:
+      "https://th.bing.com/th/id/OIP.veuJftghwFhQP487poLEtgHaEy?w=264&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    Images: [
+      "https://th.bing.com/th/id/OIP.veuJftghwFhQP487poLEtgHaEy?w=264&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    ],
+  },
+
+  {
+    Title: "Onions",
+    Prise: "$60.00",
+    Weight: "2kgs",
+    Poster:
+      "https://th.bing.com/th/id/OIP.8C8c9XnQXSHfXbueb_AZCQHaF-?w=261&h=210&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    Images: [
+      "https://th.bing.com/th/id/OIP.8C8c9XnQXSHfXbueb_AZCQHaF-?w=261&h=210&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    ],
+  },
+
+  {
+    Title: "Corn",
+    Prise: "$15.00",
+    Weight: "1pice",
+    Poster:
+      "https://th.bing.com/th/id/OIP.hd5ZpcnJDpSyQtvFmmK7vQHaE8?w=237&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    Images: [
+      "https://th.bing.com/th/id/OIP.hd5ZpcnJDpSyQtvFmmK7vQHaE8?w=237&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    ],
+  },
+
+  {
+    Title: "Grapes",
+    Prise: "$64.30",
+    Weight: "1kg",
+    Poster:
+      "https://th.bing.com/th/id/OIP.kT_FOF8p2EmNUdhggfCpqgHaDt?w=315&h=175&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    Images: [
+      "https://th.bing.com/th/id/OIP.kT_FOF8p2EmNUdhggfCpqgHaDt?w=315&h=175&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    ],
+  },
+
+  {
+    Title: "Orange",
+    Prise: "$45.25",
+    Weight: "1kg",
+    Poster:
+      "https://th.bing.com/th/id/OIP.YBq0P1bCySQrAsJpVBvdKwHaE7?w=253&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    Images: [
+      "https://th.bing.com/th/id/OIP.YBq0P1bCySQrAsJpVBvdKwHaE7?w=253&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    ],
+  },
+
+  {
+    Title: "Brinjal",
+    Prise: "$20.00",
+    Weight: "1kg",
+    Poster:
+      "https://th.bing.com/th/id/OIP.Le114eOSttQhsQvjp1MnfQAAAA?w=241&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    Images: [
+      "https://th.bing.com/th/id/OIP.Le114eOSttQhsQvjp1MnfQAAAA?w=241&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    ],
+  },
+
+   {
+     Title: "Pumpkin",
+    Prise: "$30.05",
+     Weight: "1kg",
+     Poster:
+       "https://th.bing.com/th/id/OIP.pIeIo6eySxQ_hViSg_XHrgHaFC?w=265&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+     Images: [
+       "https://th.bing.com/th/id/OIP.I6Mh--aKo_ol7m7zTeqJOQHaFL?w=190&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    ],
+   },
+
+
+
+];
+// const MainboxDiv = document.getElementById("Mainbox");
+// data.forEach(function (item) {
+//   const Mainbox = document.createElement("div");
+//   Mainbox.classList.add("Mainbox");
+//   const img = document.createElement("img");
+//   img.alt = item.img;
+
+//   const Title = document.createElement("h2");
+//   Title.textContent = item.Title;
+
+//   const Poster = item.Poster.replace("https", "http");
+//   img.setAttribute("src", Poster);
+
+//   const Prise = document.createElement("h2");
+//   Prise.textContent = item.Prise;
+
+//   const Weight = document.createElement("p");
+//   Weight.textContent = item.Weight;
+
+//   Mainbox.append(img, Title, Prise, Poster);
+//   MainboxDiv.appendChaild(Mainbox);
+// });
+
+var appendData="";
+
+for(var i=0;i<data.length;i++){
+
+    appendData +="<div>"+
+                "<div class='card'>"+                
+                "<img src='"+data[i].Poster+"'/>"+
+                "<h1>"+data[i].Title+"</h1>"+
+                "<p>"+data[i].Prise+"</p>"+
+                "<h3>"+data[i].Weight+"</h3>"+
+                
+            
+                
+                "</div></div>";
+
+
+}
+
+document.getElementById("loopData").innerHTML=appendData;
---
+++ javascript/projects/js/Hyderabad Institutes-library/script.js
@@ -0,0 +1,312 @@
+const  data=[
+    {
+        Title:"NARESH IT",
+        State:"TS,AP",
+        Country:"Indin",
+        Type:"Online/Offline",
+        Location:"Hyderabad,Ammerpat",
+        poster:
+        "https://th.bing.com/th/id/OIP.GSSyeYxS62XPFOcXdhVmLQHaEc?w=279&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+
+        Images:[
+            "https://th.bing.com/th/id/OIP.GSSyeYxS62XPFOcXdhVmLQHaEc?w=279&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+            "https://th.bing.com/th/id/OIP.ZGtCdkNfSgtA5V7TOR6qEQHaHa?w=181&h=181&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+            "https://th.bing.com/th/id/OIP.fjPQtkU-xTFGso57CGN_DAHaEK?w=267&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+        "https://th.bing.com/th/id/OIP.fjPQtkU-xTFGso57CGN_DAHaEK?w=267&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+
+        ],
+
+
+    },
+    {
+        Title:"Sathya Technologies",
+        State:"TS",
+        Country:"Indin",
+        Type:"Online/Offline",
+        Location:"Hyderabad,Ammerpat",
+        poster:
+        "https://th.bing.com/th/id/OIP._YoWz8kdYF-dDwTcX5-zSwAAAA?w=196&h=100&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+        Images:[
+            "https://th.bing.com/th/id/OIP._YoWz8kdYF-dDwTcX5-zSwAAAA?w=196&h=100&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+            "https://th.bing.com/th/id/OIP.aGCvgDRh3su10V49C_VvRwHaHa?w=184&h=185&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+            
+        ],
+
+
+    },
+    {
+        Title:"Kalyan It Training",
+        State:"TS",
+        Country:"India",
+        Type:"Online/Offline",
+        Location:"Hyderabad,Ammerpat",
+        poster:
+        "https://th.bing.com/th/id/OIP.JWmts149yA_2KRS3YuLo9gHaEK?rs=1&pid=ImgDetMain",
+        Images:[
+            "https://th.bing.com/th/id/OIP.UvoLo29PXD_HxGqHwkR2gAAAAA?w=184&h=121&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+            "https://th.bing.com/th/id/OIP.JWmts149yA_2KRS3YuLo9gHaEK?rs=1&pid=ImgDetMain",
+            
+         ],
+
+    },
+    {
+        Title:"QSHORE Technologies ",
+        State:"TS",
+        Country:"India",
+        Type:"Online",
+        Location:"Hyderabad,Ammerpat",
+        poster:
+        "https://th.bing.com/th/id/OIP.50_-suOxsfMAf-TglWnbzQAAAA?w=194&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+
+        Images:[
+            "https://th.bing.com/th/id/OIP.50_-suOxsfMAf-TglWnbzQAAAA?w=194&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+            "https://th.bing.com/th/id/OIP.L6IIIj3rsFXK09HbRYgMtgHaJ4?w=115&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+            "https://th.bing.com/th/id/OIP.eMFmkax9tZz0hx5tqQmSawHaHa?rs=1&pid=ImgDetMain",
+        ],
+
+    },
+    {
+        Title:"Quality Thought",
+        State:"TS",
+        Country:"India",
+        Type:"Online/Offline",
+        Location:'KBHP',
+        poster: "https://th.bing.com/th/id/OIP.Z47zptq2b2zZ9RZTU2J-KwHaJ4?w=130&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",       
+        Images:[
+            "https://imgv2-2-f.scribdassets.com/img/document/621487326/original/a2f119c263/1686090547?v=1",
+            "https://64.media.tumblr.com/c74676a007767811d9e248508c450f6a/db30bf7bcf38e479-38/s1280x1920/c26a2c5c49d0ec90dd8443cca63787cdcdf01ba0.jpg",
+            "https://pbs.twimg.com/media/FdFyDtYaAAEuPfu?format=jpg&name=large",
+            "https://th.bing.com/th/id/OIP.67tSx6Fh9rIT1-x9efJWgwHaHa?w=187&h=187&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+        ],
+
+    },
+    
+    {
+        Title:"Vcube",
+        State:"TS",
+        Country:"India",
+        Type:"Online/Offline",
+        Location:'KBHP',
+        poster:"https://th.bing.com/th/id/OIP.YNjUMo6qKYRX1VOuanGfRQAAAA?w=208&h=149&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+         Images:[
+            "https://www.vcubesoftsolutions.com/wp-content/uploads/2021/11/what-is-appium.png",
+            "",
+            "",
+        ],
+
+    },
+    
+    {
+        Title:"Asklaila",
+        State:"More",
+        Country:"India",
+        Type:"Online/Offline",
+        Location:'More cities',
+        poster:"https://th.bing.com/th/id/OIP.XSX3DRU8Jv8GU_RHQDKYnwHaFv?w=208&h=175&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+         Images:[
+            "https://th.bing.com/th/id/OIP.XSX3DRU8Jv8GU_RHQDKYnwHaFv?w=208&h=175&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+            "",
+            "",
+        ],
+
+    },
+    
+    {
+        Title:"Vspark",
+        State:"TS",
+        Country:"India",
+        Type:"Online/Offline",
+        Location:'Ameerpet',
+        poster:"https://th.bing.com/th/id/OIP.5_5xG-gjWLD5r3TApN_rWwHaHa?w=176&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+         Images:[
+            "https://th.bing.com/th/id/OIP.KyZxBJzNeEesGIhgqkiA1wHaC9?w=342&h=139&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+            "",
+            "",
+        ],
+
+    },
+    
+    {
+        Title:"Jspaders",
+        State:"More",
+        Country:"India",
+        Type:"Online/Offline",
+        Location:'Ameerpet,Bangalore',
+        poster:"https://th.bing.com/th/id/OIP.6BD38KWLjxMwRak1VoScDAHaDU?w=165&h=79&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+         Images:[
+            "https://lh3.googleusercontent.com/vFwoo_oDJkp4gtNHYPePAcNZ3X447CYqEhQlWuLUujggOa1rVp4gUWmJM2-Be8unsjo",
+            "https://media.licdn.com/dms/image/C560BAQEVqtIX4bs1RA/company-logo_200_200/0/1672744977058?e=2147483647&v=beta&t=Nqjw-m1jP_TjFNmjP_0Py9JPUv6llHsyZbGDWJdbBY0",
+            "",
+        ],
+
+    },
+    
+    {
+        Title:"Lamp Institute",
+        State:"TS",
+        Country:"India",
+        Type:"Online/Offline",
+        Location:'KBHP',
+        poster:"https://th.bing.com/th/id/OIP.Z2APZ8xb0a21zjHIcxOoCAHaGN?w=195&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+         Images:[
+            "https://th.bing.com/th/id/OIP.DRym0T69kWANeCP9cDFyIQAAAA?w=163&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+            
+        ],
+
+    },
+    
+    {
+        Title:"intelliPaat",
+        State:"TS",
+        Country:"India",
+        Type:"Online/Offline",
+        Location:'KBHP',
+        poster:
+               "https://th.bing.com/th/id/OIP.GoiAQDu5uGECkU1lneAVugHaHa?w=161&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+         Images:[
+            "https://th.bing.com/th/id/OIP.GoiAQDu5uGECkU1lneAVugHaHa?w=161&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+            
+        ],
+
+    },
+    { 
+    Title:"React Academy",
+    Country:"India",
+    Type:"Online/Offline",
+    Location:'Delhi,channi',
+    poster:"https://th.bing.com/th/id/OIP.lIlJgQkh_2q2eJXv72PBCQHaD3?w=296&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    Images:[
+        "https://th.bing.com/th/id/OIP.lIlJgQkh_2q2eJXv72PBCQHaD3?w=333&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+
+
+],
+    },
+    {
+    Title:"Ashok it",
+    State:"TS",
+    Country:"India",
+    Type:"Online/Offline",
+    Location:'KBHP',
+    poster:"https://th.bing.com/th/id/OIP.x-y69mwpAYhCxBUuV2gvWgAAAA?w=295&h=66&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    Images:["",
+
+
+],
+    },
+    
+    {
+    Title:"intelliPaat",
+    State:"TS",
+    Country:"India",
+    Type:"Online/Offline",
+    Location:'Ameerpet',
+    poster:
+    "https://th.bing.com/th?q=Intellipaat+Wikipedia&dc=3&w=100&h=100&c=1&rs=1&qlt=90&cb=1&dpr=1.3&pid=InlineBlock&mkt=en-IN&cc=IN&setlang=en&adlt=moderate&t=1&mw=247",
+    Images:[
+        "https://th.bing.com/th/id/OIP.x-y69mwpAYhCxBUuV2gvWgAAAA?w=280&h=66&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+        "https://th.bing.com/th/id/OIP.lIlJgQkh_2q2eJXv72PBCQHaD3?w=296&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+
+
+],
+    },
+    
+    {
+    Title:"Java institue",
+    State:"TS",
+    Country:"India",
+    Type:"Online/Offline",
+    Location:'KBHP',
+    poster:"https://media-exp1.licdn.com/dms/image/C4E0BAQEq93fFanqS5w/company-logo_200_200/0/1519873264892?e=2159024400&v=beta&t=NWZQQsVqZfyHxRnKdLiBnxKZt1lLjgNWyDkizXOPCds",
+    Images:[
+        "https://th.bing.com/th/id/OIP.78W78QNEjVyP9pI46-6INQHaDQ?w=291&h=153&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+
+
+],
+    },
+    
+    {
+    Title:"Ameerpet technologie ",
+    State:"TS",
+    Country:"India",
+    Type:"Online/Offline",
+    Location:'Ameerpet',
+    poster:"https://th.bing.com/th/id/OIP.4wQvBtd8NEt5YuluorWLxgHaHa?w=191&h=191&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    Images:[
+        "https://th.bing.com/th/id/OIP.4wQvBtd8NEt5YuluorWLxgHaHa?w=191&h=191&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+
+
+],
+    },
+    {
+        Title:"",
+        State:"",
+        Country:"",
+        Type:"",
+        Location:'',
+        poster:"",
+        Images:[
+            "",
+    
+    
+    ],
+        },
+   
+        {
+            Title:"",
+            State:"",
+            Country:"",
+            Type:"",
+            Location:'',
+            poster:"",
+            Images:[
+                "",
+        
+        
+        ],
+            },
+           
+    
+    
+    
+    
+       
+    
+]
+const couchingDiv=document.getElementById('Couching')
+data.forEach(function (couch)
+{
+    const couching = document.createElement('div')
+    couching.classList.add('couching')
+    const img = document.createElement('img')
+    img.alt=couch.img;
+    
+    const Title=document.createElement('h2')
+    Title.textContent=couch.Title;
+
+    const State=document.createElement('p')
+    State.textContent=couch.State;
+
+    const Country=document.createElement('p')
+    Country.textContent=couch.Country;
+
+    const Location=document.createElement('p')
+    Location.textContent=couch.Location;
+
+
+
+    const poster=couch.poster.replace('https','http')
+    img.setAttribute('src',poster);
+    
+    
+    const Type=document.createElement('h4')
+    Type.textContent=couch.Type;
+
+    couching.append(img,Title,Country,State,Type,Location)
+    couchingDiv.appendChild(couching)
+
+
+    
+    
+
+
+})
